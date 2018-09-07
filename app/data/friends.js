25b087f9d2f7edbc1c97ec8901bac49f818d9f27
Jacob Yolles <jacobyolles@Jacobs-MacBook-Pro.local>
2018-09-07 08:24:53 -0600

    more files

---
+++ app/data/friends.js
@@ -0,0 +1,177 @@
+// GIVE A THEME TO WHAT ALL THE FRIENDS HAVE IN COMMON. IDEA CARTOON CHARACTERS.
+// This will hold all the data for the friends possibilities. 
+var friendsArray = [
+{
+    // 1
+    name : "Spongebob Squarepants",
+    photo :"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores :[
+        "1",
+        "2",
+        "1",
+        "1",
+        "1",
+        "1",
+        "2",
+        "1",
+        "1",
+        "2",
+      ]
+  },
+  // 2
+   {
+    name : "Bob the Builder",
+    photo :"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores :[
+        "4",
+        "4",
+        "4",
+        "4",
+        "4",
+        "5",
+        "5",
+        "5",
+        "4",
+        "4",
+      ]
+  },
+  // 3
+  {
+    name : "Bugs Bunny",
+    photo :"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores :[
+        "3",
+        "3",
+        "3",
+        "3",
+        "3",
+        "3",
+        "3",
+        "3",
+        "3",
+        "3",
+      ]
+  },
+// 4
+  {
+    name : "Mickey Mouse",
+    photo :"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores :[
+        "1",
+        "2",
+        "1",
+        "1",
+        "1",
+        "1",
+        "2",
+        "1",
+        "1",
+        "2",
+      ]
+  },
+  // 5
+  {
+    name : "Donald Duck",
+    photo :"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores :[
+        "1",
+        "2",
+        "1",
+        "1",
+        "1",
+        "1",
+        "2",
+        "1",
+        "1",
+        "2",
+      ]
+  }, 
+  // 6
+  {
+    name : "Goofy",
+    photo :"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores :[
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+        "5",
+      ]
+  },
+  // 7
+  {
+    name : "Cheshire Cat",
+    photo :"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores :[
+        "4",
+        "4",
+        "4",
+        "4",
+        "4",
+        "4",
+        "4",
+        "4",
+        "4",
+        "4",
+      ]
+  },
+  // 8
+  {
+    name : "Winnie The Pooh",
+    photo :"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores :[
+        "2",
+        "2",
+        "2",
+        "2",
+        "2",
+        "2",
+        "2",
+        "2",
+        "2",
+        "2",
+      ]
+  }, 
+  // 9
+  {
+    name : "Jerry the Mouse",
+    photo :"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores :[
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+      ]
+  },
+  //10
+  {
+    name : "Mario",
+    photo :"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores :[
+        "5",
+        "4",
+        "3",
+        "2",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "4",
+      ]
+  }
+
+];// end of the friends array variable.
+
+module.exports = friendsArray;
